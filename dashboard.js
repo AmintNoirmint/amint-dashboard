a1098cd69a952980a88c12a54f640bc67f88d84d
AmintNoirmint <keemit99@icloud.com>
2025-05-16 03:41:04 -0400

    Add files via upload

---
+++ dashboard.js
@@ -0,0 +1,4 @@
+
+document.getElementById("wallet-value").innerText = "Wallet: 0x009Be1B1147...DC0 — ETH: 12.4 | AMINT: 19,875,000";
+document.getElementById("token-stats").innerText = "Total Supply: 100,000,000 AMINT | Holders: 512";
+document.getElementById("trust-ownership").innerText = "Owner: NOIRMINT INC TRUST ✅ Verified";
